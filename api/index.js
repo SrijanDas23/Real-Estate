29a7596cf5fc08da5d46c7f83b17cf7ad53e6db1
SrijanDas23 <157733127+SrijanDas23@users.noreply.github.com>
2024-03-19 12:49:12 +0530

    created sign up api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -15,7 +17,12 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("server is running at 3000");
   // nodemon api/index.js
 });
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);

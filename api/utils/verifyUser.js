15271339f50e473b59a713504847ee8b62b71cf7
SrijanDas23 <157733127+SrijanDas23@users.noreply.github.com>
2024-04-26 10:44:53 +0530

    fix 16

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -3,11 +3,10 @@ import { errorHandler } from "./error.js";
 
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
-  console.log(token);
 
   if (!token) return next(errorHandler(401, "Unauthorized"));
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+  jwt.verify(token, process.env.aelfjbajefbaj24807324, (err, user) => {
     if (err) return next(errorHandler(403, "Forbidden"));
 
     req.user = user;

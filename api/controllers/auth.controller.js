282dc09c0621e0fefdea7dd6704013722db9dcc6
SrijanDas23 <157733127+SrijanDas23@users.noreply.github.com>
2024-04-26 09:44:15 +0530

    fix 11

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -30,14 +30,7 @@ export const signIn = async (req, res, next) => {
     if (!validPassword) return next(errorHandler(401, "Invalid Credentials"));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: pass, ...rest } = validUser._doc;
-    res
-      .cookie("access_token", token, {
-        httpOnly: true,
-        secure: true,
-        sameSite: "none",
-      })
-      .status(200)
-      .json(rest);
+    res.cookie("access_token", token).status(200).json(rest);
   } catch (error) {
     next(error);
   }
@@ -49,14 +42,7 @@ export const google = async (req, res, next) => {
     if (user) {
       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
       const { password: pass, ...rest } = user._doc;
-      res
-        .cookie("access_token", token, {
-          httpOnly: true,
-          secure: true,
-          sameSite: "none",
-        })
-        .status(200)
-        .json(rest);
+      res.cookie("access_token", token).status(200).json(rest);
     } else {
       const generatedPassword =
         Math.random().toString(36).slice(-8) +
@@ -73,14 +59,7 @@ export const google = async (req, res, next) => {
       await newUser.save();
       const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
       const { password: pass, ...rest } = newUser._doc;
-      res
-        .cookie("access_token", token, {
-          httpOnly: true,
-          secure: true,
-          sameSite: "none",
-        })
-        .status(200)
-        .json(rest);
+      res.cookie("access_token", token).status(200).json(rest);
     }
   } catch (error) {
     next(error);
